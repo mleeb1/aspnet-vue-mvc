dfc79ce291209d9206b52cc81c4a465ec00709b7
Mel Leeb <mel.leeb@sherwin.com>
2019-09-03 08:34:55 -0400

    Init commit

---
+++ VueMVC/Scripts/app/home/FirstComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="time">
+        <h2>Hello 3</h2>
+        {{ time }}
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            time: {
+                type: Object,
+                required: true, 
+            }
+        }
+    }
+</script>
+<style>
+    .time {
+        color: #F44336;
+    }
+</style>
